7d9a046a1b929086c5d83d5beb5fa31a7f653795
suraj pro <71000909+suraj719@users.noreply.github.com>
2024-07-18 00:47:54 +0530

    feat: dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DefaultLayout from "./layout/DefaultLayout";
 import Incidents from "./pages/Incidents";
 import Resources from "./pages/Resources";
 import Members from "./pages/Members";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <DefaultLayout>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/incidents" element={<Incidents />} />
           <Route path="/resources" element={<Resources />} />
           <Route path="/members" element={<Members />} />
